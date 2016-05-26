0be300f49f07b50e895a3b4901d4c08a84df9f0b
JessicaYorzinski <jly8@cornell.edu>
2016-05-25 23:48:20 -0400

    auto scaling

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ OVAL_RADIUS = 4
 FRAME_COL = 0
 X_COOR_COL = 5
 Y_COOR_COL = 6
-MAX_X = 480
-MAX_Y = 480
 work_dir = '../02/'
 
 
-def draw_oval(img, oval_radius, oval_center, oval_color):
+def draw_oval(img, oval_radius, oval_center, oval_color, height, width):
     '''
     Draws a yellow oval at the given coordinates.
     :param img:
@@ -23,11 +21,11 @@ def draw_oval(img, oval_radius, oval_center, oval_color):
     for x in range(oval_center[0]-oval_radius, oval_center[0]+oval_radius):
         for y in range(oval_center[1]-oval_radius, oval_center[1]+oval_radius):
             if (x-oval_center[0])*(x-oval_center[0]) + (y-oval_center[1])*(y-oval_center[1]) <= oval_radius*oval_radius:
-                if x < MAX_X and y < MAX_Y:
+                if x < height and y < width:
                     img[x, y] = oval_color
 
 
-def load_coordinates(input_path):
+def load_coordinates(input_path, height, width):
     '''
     Loads the corrdinates of the yellow dot
 
@@ -52,10 +50,10 @@ def load_coordinates(input_path):
         for i in range(7, len(lines)):
             line = lines[i]
             vals = line.split(' ')
-            res[int(vals[FRAME_COL])] = (int((float(vals[X_COOR_COL]) - minX)*(MAX_X-1)/(maxX-minX)), int((float(vals[Y_COOR_COL]) - minY)*(MAX_Y-1)/(maxY-minY)))
+            res[int(vals[FRAME_COL])] = (int((float(vals[X_COOR_COL]) - minX)*(height-1)/(maxX-minX)), int((float(vals[Y_COOR_COL]) - minY)*(width-1)/(maxY-minY)))
     return res
 
-coors = load_coordinates(work_dir+'05-19-12_No53_363301_Cop.txt')
+
 
 vidcap = cv2.VideoCapture(work_dir+'05-19-12_No53_363301_Cop.mov')
 frame_num = 1
@@ -63,6 +61,7 @@ success, image = vidcap.read()
 
 # init the video writer
 height, width, layers = image.shape
+coors = load_coordinates(work_dir+'05-19-12_No53_363301_Cop.txt', height, width)
 fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
 output = cv2.VideoWriter(work_dir+'output.avi', fourcc, 60, (width, height))
 
@@ -71,7 +70,7 @@ while success:
     if frame_num in coors:
         x, y = coors[frame_num]
         # do you calculations here
-        draw_oval(image, OVAL_RADIUS, (x, y), OVAL_COLOR)
+        draw_oval(image, OVAL_RADIUS, (x, y), OVAL_COLOR, height, width)
     output.write(image)
     frame_num += 1
     success, image = vidcap.read()
